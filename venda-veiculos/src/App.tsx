15e254a49a0e6ba4d71c98a33d4f55b1c494735f
ElianoCordeiro <eliano@gmail.com>
2023-06-10 19:33:34 -0300

    Initial Config

---
+++ venda-veiculos/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div class="main">
+      <h2>Venda Veículos</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
