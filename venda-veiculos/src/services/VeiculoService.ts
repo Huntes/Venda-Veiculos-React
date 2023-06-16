4934169fbeb39c7fac4f575ca1afc9f7cb38fdbb
ElianoCordeiro <eliano@gmail.com>
2023-06-16 03:36:35 -0300

    Correções Telas - Services

--- venda-veiculos/src/services/VeiculoService.ts
+++ venda-veiculos/src/services/VeiculoService.ts
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import { Car } from '../types/Car';
-import { useRequest } from './BaseService';
 import axios from 'axios';
 
 const token = localStorage.getItem('token');
@@ -36,32 +34,32 @@ const headerGetRequest = {
 
 const GetAll = async () => {
    
-    return sendRequest<Car[]>('GET', 'https://localhost:5501/api/Vehicles/getAll', undefined, headerGetRequest);
+    return sendRequest<Car[]>('GET', `${import.meta.env.VITE_API_URL}/api/Vehicles/getAll`, undefined, headerGetRequest);
 }
 
 const Get = async (id: string) => {
     
-    return await sendRequest<Car>('GET', `https://localhost:5501/api/Vehicles/get/${id}`, undefined, headerGetRequest);
+    return await sendRequest<Car>('GET', `${import.meta.env.VITE_API_URL}/api/Vehicles/get/${id}`, undefined, headerGetRequest);
 }
 
 const Insert = async (car: Car) => {
    
-    return await sendRequest<Car>('POST', 'https://localhost:5501/api/Vehicles/insert-car', car, headerSendRequest);
+    return await sendRequest<Car>('POST', `${import.meta.env.VITE_API_URL}/api/Vehicles/insert-car`, car, headerSendRequest);
 }
 
 const Update = async (id: string, car: Car) => {
    
-    return await sendRequest<Car>('PUT', `https://localhost:5501/api/Vehicles/update/${id}`, car, headerSendRequest);
+    return await sendRequest<Car>('PUT', `${import.meta.env.VITE_API_URL}/api/Vehicles/update/${id}`, car, headerSendRequest);
 }
 
 const ToggleCar = async (id: string) => {
     
-    return await sendRequest('PUT', `https://localhost:5501/api/Vehicles/change-status/${id}`, undefined, headerSendRequest);
+    return await sendRequest('PUT', `${import.meta.env.VITE_API_URL}/api/Vehicles/change-status/${id}`, undefined, headerSendRequest);
 }
 
 const Delete = async (id: string) => {
     
-    return await sendRequest('DELETE', `https://localhost:5501/api/Vehicles/delete/${id}`, null, headerSendRequest);
+    return await sendRequest('DELETE', `${import.meta.env.VITE_API_URL}/api/Vehicles/delete/${id}`, null, headerSendRequest);
 }
 
 //Cria um objeto que contém as funções de acesso ao backend
